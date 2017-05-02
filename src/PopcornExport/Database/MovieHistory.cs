fa19ea309be89b04109f3c4b2df8ac559d14072b
Benjamin Bougot <bbougot@gmail.com>
2017-05-02 22:16:34 +0200

    Add user to database

---
+++ src/PopcornExport/Database/MovieHistory.cs
@@ -0,0 +1,14 @@
+﻿namespace PopcornExport.Database
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class MovieHistory
+    {
+        public int Id { get; set; }
+
+        public string ImdbId { get; set; }
+        public bool Seen { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
--- src/PopcornExport/Migrations/20170502193548_User.Designer.cs
+++ src/PopcornExport/Migrations/20170502193548_User.Designer.cs
@@ -8,9 +8,10 @@ using PopcornExport.Database;
 namespace PopcornExport.Migrations
 {
     [DbContext(typeof(PopcornContext))]
-    partial class PopcornContextModelSnapshot : ModelSnapshot
+    [Migration("20170502193548_User")]
+    partial class User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
