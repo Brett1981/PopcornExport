aea2a2655e09c4658d0e2995fe4b3a2180bb2373
Benjamin Bougot <bbougot@gmail.com>
2018-04-23 23:28:24 +0200

    Fixed issue

--- PopcornExport/Schim/ProgressBar.cs
+++ PopcornExport/Schim/ProgressBar.cs
@@ -9,46 +9,10 @@ namespace PopcornExport.Schim
     {
         public static IProgressBar Create(int maxTicks, string message, ProgressBarOptions options)
         {
-            if(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPSETTING_WEBSITE_SITE_NAME")))
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPSETTING_WEBSITE_SITE_NAME")))
                 return new ShellProgressBar.ProgressBar(maxTicks, message, options);
 
             return new DummyProgressBar();
-        }
-    }
-
-    public class DummyChildProgressBar : ChildProgressBar
-    {
-        public int MaxTicks { get; set; }
-        public string Message { get; set; }
-        public double Percentage { get; private set; }
-        public int CurrentTick { get; private set; }
-        public ConsoleColor ForeGroundColor { get; }
-
-        public DummyChildProgressBar(int maxTicks, string message, Action scheduleDraw, ProgressBarOptions options = null, Action<ProgressBarHeight> growth = null):
-            base(maxTicks, message, scheduleDraw, options, growth)
-        {
-            
-        }
-
-        public new void Dispose()
-        {
-            
-        }
-
-        public new void Tick(string message = "")
-        {
-            CurrentTick++;
-            Percentage = (double)CurrentTick / (double)MaxTicks * 100d;
-            Console.WriteLine($"{Percentage}%");
-        }
-
-        public new ChildProgressBar Spawn(int maxTicks, string message, ProgressBarOptions options = null)
-        {
-            Console.WriteLine(message);
-            MaxTicks = maxTicks;
-            CurrentTick = 0;
-            Message = message;
-            return new DummyChildProgressBar(maxTicks, message, () => { }, options);
         }
     }
 
@@ -60,18 +24,11 @@ namespace PopcornExport.Schim
 
         public ChildProgressBar Spawn(int maxTicks, string message, ProgressBarOptions options = null)
         {
-            Console.WriteLine(message);
-            MaxTicks = maxTicks;
-            CurrentTick = 0;
-            Message = message;
-            return new DummyChildProgressBar(maxTicks, message, () => { }, options);
+            return null;
         }
 
         public void Tick(string message = "")
         {
-            CurrentTick++;
-            Percentage = (double) CurrentTick / (double) MaxTicks * 100d;
-            Console.WriteLine($"{Percentage}%");
         }
 
         public int MaxTicks { get; set; }
