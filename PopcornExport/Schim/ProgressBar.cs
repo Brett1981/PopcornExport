9ccdadd9826e4335b5206a19e51a16fbe7ed3c97
Benjamin Bougot <bbougot@gmail.com>
2018-04-23 22:45:43 +0200

    Fix issues

--- PopcornExport/Schim/ProgressBar.cs
+++ PopcornExport/Schim/ProgressBar.cs
@@ -16,11 +16,46 @@ namespace PopcornExport.Schim
         }
     }
 
+    public class DummyChildProgressBar : ChildProgressBar
+    {
+        public int MaxTicks { get; set; }
+        public string Message { get; set; }
+        public double Percentage { get; private set; }
+        public int CurrentTick { get; private set; }
+        public ConsoleColor ForeGroundColor { get; }
+
+        public DummyChildProgressBar(int maxTicks, string message, Action scheduleDraw, ProgressBarOptions options = null, Action<ProgressBarHeight> growth = null):
+            base(maxTicks, message, scheduleDraw, options, growth)
+        {
+            
+        }
+
+        public new void Dispose()
+        {
+            
+        }
+
+        public new void Tick(string message = "")
+        {
+            CurrentTick++;
+            Percentage = (double)CurrentTick / (double)MaxTicks * 100d;
+            Console.WriteLine($"{Percentage}%");
+        }
+
+        public new ChildProgressBar Spawn(int maxTicks, string message, ProgressBarOptions options = null)
+        {
+            Console.WriteLine(message);
+            MaxTicks = maxTicks;
+            CurrentTick = 0;
+            Message = message;
+            return new DummyChildProgressBar(maxTicks, message, () => { }, options);
+        }
+    }
+
     public class DummyProgressBar : IProgressBar
     {
         public void Dispose()
         {
-            
         }
 
         public ChildProgressBar Spawn(int maxTicks, string message, ProgressBarOptions options = null)
@@ -29,11 +64,12 @@ namespace PopcornExport.Schim
             MaxTicks = maxTicks;
             CurrentTick = 0;
             Message = message;
-            return null;
+            return new DummyChildProgressBar(maxTicks, message, () => { }, options);
         }
 
         public void Tick(string message = "")
         {
+            CurrentTick++;
             Percentage = (double) CurrentTick / (double) MaxTicks * 100d;
             Console.WriteLine($"{Percentage}%");
         }
